c5496ca4996905cde7a0ed03a24f7c59c7285e56
Cr7OrLeo <jorge.junior.adulto@gmail.com>
2025-08-26 20:19:49 +0100

    changed a small problem

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,6 @@ from .serializers import PostSerializer
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
+    def create(self, request, *args, **kwargs):
+        print(request.FILES)  # Debug line
+        return super().create(request, *args, **kwargs)
