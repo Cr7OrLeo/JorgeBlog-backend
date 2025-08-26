776e68705427c7d88d7934fab990b7efa72943ea
Cr7OrLeo <jorge.junior.adulto@gmail.com>
2025-08-26 19:28:27 +0100

    Finally i think i fixed it

--- blog/serializers.py
+++ blog/serializers.py
@@ -2,13 +2,13 @@ from rest_framework import serializers
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
-    file = serializers.SerializerMethodField()
+    file_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = "__all__"
+        fields = "__all__"  # now includes file_url as well
 
-    def get_file(self, obj):
+    def get_file_url(self, obj):
         request = self.context.get("request")
         if obj.file:
             return request.build_absolute_uri(obj.file.url)
