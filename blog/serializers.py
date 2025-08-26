70cb66d9f90de9a39fa3f379d6b883d5cb6aebc8
Cr7OrLeo <jorge.junior.adulto@gmail.com>
2025-08-26 16:40:11 +0100

    Fixed bugs

--- blog/serializers.py
+++ blog/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
+    file = serializers.SerializerMethodField()
+
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = "__all__"
+
+    def get_file(self, obj):
+        request = self.context.get("request")
+        if obj.file:
+            return request.build_absolute_uri(obj.file.url)
+        return None
