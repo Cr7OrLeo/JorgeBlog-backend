78f2331b41c887b8a5e0dc6b9d0a1f12a05a4c8c
Cr7OrLeo <jorge.junior.adulto@gmail.com>
2025-08-27 12:56:20 +0100

    changed serializers

--- blog/serializers.py
+++ blog/serializers.py
@@ -2,14 +2,6 @@ from rest_framework import serializers
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
-    file = serializers.SerializerMethodField()
-
     class Meta:
         model = Post
-        fields = "__all__"  # now includes file as well
-
-    def get_file(self, obj):
-        request = self.context.get("request")
-        if obj.file:
-            return request.build_absolute_uri(obj.file.url)
-        return None
+        fields = "__all__"  # includes file
