32e4316a3abe9b9f5e7aa9165613fb9a54b6ad96
Cr7OrLeo <jorge.junior.adulto@gmail.com>
2025-08-27 11:54:59 +0100

    fixed a line of code that was missing

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from rest_framework import viewsets
 from .models import Post
 from .serializers import PostSerializer
+from rest_framework.parsers import MultiPartParser, FormParser
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
+    parser_classes = (MultiPartParser, FormParser)
+
     def create(self, request, *args, **kwargs):
         print(request.FILES)  # Debug line
         return super().create(request, *args, **kwargs)
